fcbe9558859658160a40b4438a1a5f772b4c668a
JuanSuppicich <suppicichjuan@gmail.com>
2021-07-23 16:54:36 -0300

    Add Spring Initializr

---
+++ src/test/java/com/example/durandsuppicich/dangateway/DanGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.durandsuppicich.dangateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
